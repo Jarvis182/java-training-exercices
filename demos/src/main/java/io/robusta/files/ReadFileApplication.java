8b45bb5e3c4189d0709f9df8dce1a62fcf9c1d4d
Jarvis182 <patrice.hanne@gmail.com>
2016-12-02 11:36:37 +0100

    preparing conversion md to html, and jar

---
+++ demos/src/main/java/io/robusta/files/ReadFileApplication.java
@@ -0,0 +1,82 @@
+package io.robusta.files;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadFileApplication {
+
+	public static void main(String[] args) {
+
+		ReadFileApplication app = new ReadFileApplication();
+		try {
+			app.readFile(args[0], args[1]);
+		} catch (IOException e) {
+			System.out.println("Oups, le frichier" + args[0] + "n'existe pas");
+		}
+
+	}catch(
+
+	IOException e)
+	{
+		System.out.println("Oups, aie aie aie I Fucked it up! GAME OVER");
+	}
+
+	void readFile(String argumentFile, String output) throws IOException {
+
+		// WHERE AM I ?
+
+		String executionPath = System.getProperty("user.dir");
+		System.out.println(executionPath);
+		String textPath = executionPath + "/" + argumentFile;
+		Path path = Paths.get(textPath);
+
+		List<String> lines = Files.readAllLines(path);
+		List<String> resultFile = new ArrayList<>();
+
+		// modifie le fichier de depart
+		for (int index = 0; index < lines.size(); index++) {
+			String current = lines.get(index);
+
+			if (current.length() > 0 && current.charAt(0) == '*') {
+				String result = current.replaceFirst("\\*", "<li>");
+				System.out.println(result);
+				resultFile.add(result);
+
+			}
+		}
+
+		for (int i = 0; i < resultFile.size(); i++) {
+			System.out.println(resultFile.get(i));
+
+		}
+		// ABSOLUTELY EQUIVALENT
+		for (String s : resultFile) {
+			System.out.println(s);
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (String line : resultFile) {
+			sb.append(line).append("\n");
+
+		}
+
+		String resultPath = executionPath + "/" + output;
+
+		// ecrit a la fin
+		Files.write(Paths.get(resultPath), sb.toString().getBytes("utf-8"), StandardOpenOption.CREATE,
+				StandardOpenOption.TRUNCATE_EXISTING);
+
+		// java.nio.file.Files.write(Paths.get(file.toURI()), "My string to
+		// save".getBytes("utf-8"), StandardOpenOption.CREATE,
+		// StandardOpenOption.TRUNCATE_EXISTING);
+
+		System.out.println(lines);
+
+	}
+
+}
